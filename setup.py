b928c412c5847287990c96baf210e67e02f00381
Dhruv Tyagi <dhruvtyagi203@gmail.com>
2023-09-29 14:45:02 +0530

    commit

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="src",
     version="0.0.1",
-    author="Dhruv",
-    author_email="dhruvtyagi203@gmail.com",
+    author="iNeuron",
+    author_email="cloud@ineuron.ai",
     packages=find_packages(),
-    install_requires=[]
+    install_requires=[],
 )
